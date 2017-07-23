221950668ead39004071b8052062a41dfdc93875
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2017-07-23 10:25:09 +0900

    占い結果に対する処理を実装

--- src/java/glyAiWolf/player/BasePlayer.java
+++ src/java/glyAiWolf/player/BasePlayer.java
@@ -1,43 +1,58 @@
 package glyAiWolf.player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
+import org.aiwolf.client.lib.Content;
 import org.aiwolf.common.data.Agent;
 import org.aiwolf.common.data.Player;
 import org.aiwolf.common.data.Role;
+import org.aiwolf.common.data.Species;
+import org.aiwolf.common.data.Talk;
+import org.aiwolf.common.data.Team;
 import org.aiwolf.common.net.GameInfo;
 import org.aiwolf.common.net.GameSetting;
 
 public class BasePlayer implements Player {
+	protected List<Role> villagerSide = new ArrayList<>();
+	protected List<Role> werewolfSide = new ArrayList<>();
+	// 日ごとのgameInfo
 	protected Map<Integer, GameInfo> gameInfos = new TreeMap<>();
+	// 今回のゲームのgameSetting, initialize時にコピーし，後はそのまま
 	protected GameSetting gameSetting;
+	protected BlockingDeque<Talk> processedTalks = new LinkedBlockingDeque<>();
+	// 各プレイヤーの各役職の可能性
 	protected double[][] rolePossibility;
 
+	/**
+	 * BasePlayerでは実装しない
+	 */
 	@Override
 	public Agent attack() {
-		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
 	@Override
 	public void dayStart() {
 		// TODO 自動生成されたメソッド・スタブ
-
 	}
 
+	/**
+	 * BasePlayerでは実装しない
+	 */
 	@Override
 	public Agent divine() {
-		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
 	@Override
 	public void finish() {
 		// TODO 自動生成されたメソッド・スタブ
-
 	}
 
 	/**
@@ -91,14 +106,106 @@ public class BasePlayer implements Player {
 
 	@Override
 	public String getName() {
-		// TODO 自動生成されたメソッド・スタブ
+		return "glycine";
+	}
+
+	/**
+	 * BasePlayerでは実装しない
+	 */
+	@Override
+	public Agent guard() {
 		return null;
 	}
 
-	@Override
-	public Agent guard() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	/**
+	 * Comingout発言の処理．
+	 * <ol>
+	 * <li>村/狼サイドの役職中の可能性が集約される</li>
+	 * <li>もし可能性がない役職についてCOしている場合は狼サイドと判定</li>
+	 * <li>他サイドについては考慮しない</li>
+	 * </ol>
+	 * TODO: 他サイドのCOの処理
+	 * 
+	 * @param content
+	 */
+	protected void handleComingout(Agent agent, Content content) {
+		int playerIndex = agent.getAgentIdx() - 1;
+		Role coRole = content.getRole();
+		List<Role> coTeamRoles = Arrays.asList(Arrays.asList(Role.values()).stream()
+				.filter(x -> x.getTeam().equals(coRole.getTeam())).filter(y -> !y.equals(coRole)).toArray(Role[]::new));
+		double prob = 0.0;
+		for (Role role : coTeamRoles) {
+			prob += this.rolePossibility[playerIndex][role.ordinal()];
+			this.rolePossibility[playerIndex][role.ordinal()] = 0.0;
+		}
+
+		if (this.rolePossibility[playerIndex][coRole.ordinal()] > 0.0) {
+			// 村人サイドの可能性がある
+			this.rolePossibility[playerIndex][coRole.ordinal()] += prob;
+		} else {
+			// 村人サイドの可能性がない->現在は狼の可能性が高いと判定する
+			this.rolePossibility[playerIndex][Role.WEREWOLF.ordinal()] += prob;
+		}
+	}
+
+	/**
+	 * 占い結果を処理する．
+	 * - 占い結果が真実の可能性がある場合は，占われた対象がそのteamSideである可能性を上げる．
+	 * - 上げる割合は1/2
+	 * - 占い結果が偽である場合は，占った対象を狼の割合を上げる
+	 * 
+	 * @param agent
+	 * @param content
+	 */
+	protected void handleDevined(Agent agent, Content content) {
+		int playerIndex = agent.getAgentIdx() - 1;
+		int targetIndex = content.getTarget().getAgentIdx() - 1;
+		Species species = content.getResult();
+		// まず判定されたspeciesである可能性を判定する
+		Role[] targetRoles = Arrays.asList(Role.values()).stream().filter(x -> x.getSpecies().equals(species))
+				.toArray(Role[]::new);
+		boolean checkResult = false;
+		for (Role targetRole : targetRoles) {
+			if (this.rolePossibility[targetIndex][targetRole.ordinal()] > 0.0) {
+				checkResult = true;
+				break;
+			}
+		}
+		if (checkResult) {
+			// 占い結果はあり得る -> 占い結果を踏まえて確率を変える
+			// 村サイドの確率をp, 狼サイドの確率が1-pであり，占い師の確率がqであるときに，村サイドの占い結果が出たとすると，
+			// 新しい占い結果は，p + (1-p) * qとする
+			double seerProb = this.rolePossibility[playerIndex][Role.SEER.ordinal()];
+			double targetProb = 0.0;
+			for (Role targetRole : targetRoles) {
+				targetProb += this.rolePossibility[targetIndex][targetRole.ordinal()];
+			}
+			double opposeProb = 1.0 - targetProb;
+			double diff = Math.abs(targetProb - opposeProb);
+
+			System.err.println("seerProb: " + seerProb + ", targetProb: " + targetProb + ", opposeProb: " + opposeProb
+					+ ", diff: " + diff);
+			for (Role targetRole : targetRoles) {
+				this.rolePossibility[targetIndex][targetRole.ordinal()] += opposeProb
+						* this.rolePossibility[targetIndex][targetRole.ordinal()] / targetProb * seerProb;
+			}
+			Role[] opposeRoles = Arrays.asList(Role.values()).stream().filter(x -> !x.getSpecies().equals(species))
+					.toArray(Role[]::new);
+			for (Role opposeRole : opposeRoles) {
+				this.rolePossibility[targetIndex][opposeRole
+						.ordinal()] -= this.rolePossibility[targetIndex][opposeRole.ordinal()] * seerProb;
+			}
+		} else {
+			// 占い結果はありえない -> 占った人は狼の可能性が高いと判定する
+			Role[] villagerRoles = Arrays.asList(Role.values()).stream()
+					.filter(x -> x.getSpecies().equals(Species.HUMAN)).toArray(Role[]::new);
+			double prob = 0.0;
+			for (Role role : villagerRoles) {
+				prob += this.rolePossibility[playerIndex][role.ordinal()];
+				this.rolePossibility[playerIndex][role.ordinal()] = 0.0;
+			}
+			this.rolePossibility[playerIndex][Role.WEREWOLF.ordinal()] += prob;
+		}
 	}
 
 	/**
@@ -106,6 +213,14 @@ public class BasePlayer implements Player {
 	 */
 	@Override
 	public void initialize(GameInfo gameInfo, GameSetting gameSetting) {
+		for (Role role : Role.values()) {
+			System.err.println(role + ", " + role.getSpecies() + ", " + role.getTeam());
+		}
+		// 前の情報を引き継がないようにするためクリア
+		this.gameInfos.clear();
+		this.processedTalks.clear();
+
+		// 値のコピー
 		this.gameInfos.put(gameInfo.getDay(), gameInfo);
 		this.gameSetting = gameSetting.clone();
 		// 各役職の可能性の行列を作成する
@@ -117,6 +232,84 @@ public class BasePlayer implements Player {
 			}
 			System.err.println("");
 		}
+	}
+
+	/**
+	 * 他プレイヤーのtalkを処理する
+	 * 
+	 * @param talk
+	 */
+	protected void handleTalk(Talk talk) {
+		Agent agent = talk.getAgent();
+		Content content = new Content(talk.getText());
+		switch (content.getTopic()) {
+		case AGREE:
+			break;
+		case ATTACK:
+			break;
+		case COMINGOUT:
+			handleComingout(agent, content);
+			break;
+		case DISAGREE:
+			break;
+		case DIVINATION:
+			break;
+		case DIVINED:
+			this.handleDevined(agent, content);
+			break;
+		case ESTIMATE:
+			break;
+		case GUARD:
+			break;
+		case GUARDED:
+			break;
+		case IDENTIFIED:
+			break;
+		case OPERATOR:
+			break;
+		case OVER:
+			break;
+		case SKIP:
+			break;
+		case VOTE:
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public String talk() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	/**
+	 * 配信されるgameInfoを保存
+	 */
+	@Override
+	public void update(GameInfo gameInfo) {
+		this.gameInfos.put(gameInfo.getDay(), gameInfo);
+		BlockingDeque<Talk> newTalks = new LinkedBlockingDeque<>();
+		for (Talk talk : gameInfo.getTalkList()) {
+			if (processedTalks.isEmpty() || !talk.equals(processedTalks.getLast())) {
+				newTalks.addFirst(talk);
+			}
+		}
+		System.err.println("newTalksSize: " + newTalks.size());
+		while (!newTalks.isEmpty()) {
+			Talk talk = newTalks.pollFirst();
+			handleTalk(talk);
+			this.processedTalks.addFirst(talk);
+		}
+		System.err.println("processedTalksSize: " + processedTalks.size());
+		for (double[] row : rolePossibility) {
+			for (double elem : row) {
+				System.err.print(elem + ", ");
+			}
+			System.err.println("");
+		}
+
 	}
 
 	/**
@@ -132,28 +325,16 @@ public class BasePlayer implements Player {
 	}
 
 	@Override
-	public String talk() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
-	/**
-	 * 配信されるgameInfoを保存
-	 */
-	@Override
-	public void update(GameInfo gameInfo) {
-		this.gameInfos.put(gameInfo.getDay(), gameInfo);
-	}
-
-	@Override
 	public Agent vote() {
 		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
+	/**
+	 * BasePlayerでは実装しない
+	 */
 	@Override
 	public String whisper() {
-		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
 
